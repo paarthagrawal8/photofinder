4217aa79cb3bf6c0f6a783ec2edf0c096501e090
paarth <paarthagrawal8@gmail.com>
2021-04-21 13:28:55 +0530

    api called

--- photoFinder/ViewController.swift
+++ photoFinder/ViewController.swift
@@ -7,13 +7,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct apiresponse: Codable {
+    let total: Int
+    let total_pages : Int
+    let results:[Result]
+}
 
+struct Result : Codable{
+    let id : String
+    let urls : URLS
+    }
+struct URLS: Codable {
+    let full: String
+    
+}
+class ViewController: UIViewController {
+    let urlstring  = "https://api.unsplash.com/search/collections?page=50&query=office&client_id=-m__cHmERkdS79ipLOHoaKESobSwI9GD2MVwnwWqs6o"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        fetchphotos()
+        }
+        
+    func fetchphotos() {
+        guard let url = URL(string: urlstring) else {
+            return
+            }
+        let task = URLSession.shared.dataTask(with: url) {data , _ , error in  guard let data = data , error == nil else {
+            return
+            }
+        do {
+            let jsonresults = try JSONDecoder().decode(apiresponse.self, from: data)
+            print(jsonresults.results.count)
+            }
+
+        catch {
+        print(error)
+            }
+        };task.resume()
     }
-
-
 }
+    
+        // Do any additional setup after loading the view.
+    
+
+        
+    
+    
+
+
 
